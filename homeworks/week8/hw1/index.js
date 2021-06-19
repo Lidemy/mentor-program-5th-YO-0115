54db68efd8e4b43e1298d1f0f0ed38f3f5e1743a
YO-0115 <1024sone@gmail.com>
2021-06-19 11:40:22 +0800

    week8Update

--- homeworks/week8/hw1/index.js
+++ homeworks/week8/hw1/index.js
@@ -13,7 +13,22 @@ document.querySelector('.btn').addEventListener('click', (e) => {
         alert(errorMessage)
         return
       }
-      getPrize(data)
+      switch (data) {
+        case 'FIRST':
+          renderPrize('prize-first', '恭喜你中頭獎了！日本東京來回雙人遊！')
+          break
+        case 'SECOND':
+          renderPrize('prize-second', '二獎！90 吋電視一台！')
+          break
+        case 'THIRD':
+          renderPrize('prize-third', '恭喜你抽中三獎：知名 YouTuber 簽名握手會入場券一張，bang！')
+          break
+        case 'NONE':
+          renderPrize('prize-none', '銘謝惠顧')
+          break
+        default:
+          renderPrize('prize-boom', '系統不穩定，請再試一次')
+      }
     } else {
       alert(errorMessage)
     }
@@ -25,50 +40,15 @@ document.querySelector('.btn').addEventListener('click', (e) => {
   request.send()
 })
 
-function getPrize(data) {
+function renderPrize(classname, text) {
   const div = document.createElement('div')
-
-  if (data === 'FIRST') {
-    div.classList.add('prize-first')
-    div.innerHTML = ` 
+  div.classList.add(classname)
+  div.innerHTML = ` 
       <div class="card__prize">
-      <p class="card__prize-titel">恭喜你中頭獎了！日本東京來回雙人遊！</p>
+      <p class="card__prize-titel">${text}</p>
       <button class="btn btn__prize" onclick="javascript:window.location.reload()">我要抽獎</button>
       </div>
     `
-  } else if (data === 'SECOND') {
-    div.classList.add('prize-second')
-    div.innerHTML = ` 
-      <div class="card__prize">
-      <p class="card__prize-titel">二獎！90 吋電視一台！</p>
-      <button class="btn btn__prize" onclick="javascript:window.location.reload()">我要抽獎</button>
-      </div>
-    `
-  } else if (data === 'THIRD') {
-    div.classList.add('prize-third')
-    div.innerHTML = ` 
-      <div class="card__prize">
-      <p class="card__prize-titel">恭喜你抽中三獎：知名 YouTuber 簽名握手會入場券一張，bang！</p>
-      <button class="btn btn__prize" onclick="javascript:window.location.reload()">我要抽獎</button>
-      </div>
-    `
-  } else if (data === 'NONE') {
-    div.classList.add('prize-none')
-    div.innerHTML = ` 
-      <div class="card__prize">
-      <p class="card__prize-titel">銘謝惠顧</p>
-      <button class="btn btn__prize" onclick="javascript:window.location.reload()">我要抽獎</button>
-      </div>
-    `
-  } else {
-    div.classList.add('prize-boom')
-    div.innerHTML = ` 
-      <div class="card__prize">
-      <p class="card__prize-titel">系統不穩定，請再試一次</p>
-      <button class="btn btn__prize" onclick="javascript:window.location.reload()">我要抽獎</button>
-      </div>
-    `
-  }
   const section = document.querySelector('.section')
   const bg = document.querySelector('.bg')
   section.appendChild(div)
